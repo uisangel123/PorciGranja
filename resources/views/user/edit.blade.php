efee9c634a8a53551b0e6a474e861cee3b94a7c1
LuisAngel <132675523+uisangel123@users.noreply.github.com>
2023-09-01 10:25:31 -0500

    e arregla la vista usuario

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -9,30 +9,7 @@
 
     @include('layouts.menu')
     <main id="main" class="main">
-        <section class="content container-fluid">
-            <div class="">
-                <div class="col-md-12">
 
-                    @includeif('partials.errors')
-
-                    <div class="card card-default">
-                        <div class="card-header">
-                            <span class="card-title">{{ __('Update') }} User</span>
-                        </div>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('users.update', $user->id) }}" role="form"
-                                enctype="multipart/form-data">
-                                {{ method_field('PATCH') }}
-                                @csrf
-
-                                @include('user.form')
-
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
         <div class="pagetitle">
             <h1>Perfil</h1>
@@ -52,7 +29,7 @@
                     <div class="card">
                         <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
-                            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                            <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
                             <h2>{{ $user->name }}</h2>
                             <h3>{{ $user->rol }}</h3>
                             <div class="social-links mt-2">
@@ -73,18 +50,10 @@
                             <!-- Bordered Tabs -->
                             <ul class="nav nav-tabs nav-tabs-bordered">
 
-                                <li class="nav-item">
-                                    <button class="nav-link active" data-bs-toggle="tab"
-                                        data-bs-target="#profile-overview">Overview</button>
-                                </li>
+                               
 
                                 <li class="nav-item">
                                     <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Editar Perfil</button>
-                                </li>
-
-                                <li class="nav-item">
-                                    <button class="nav-link" data-bs-toggle="tab"
-                                        data-bs-target="#profile-settings">Settings</button>
                                 </li>
 
                                 <li class="nav-item">
@@ -95,52 +64,21 @@
                             </ul>
                             <div class="tab-content pt-2">
 
-                                <div class="tab-pane fade show active profile-overview" id="profile-overview">
-                                    <h5 class="card-title">About</h5>
-                                    <p class="small fst-italic">que puede hacer el administrador  accusantium neque nam maiores
-                                        cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure
-                                        rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at
-                                        unde.</p>
 
-                                    <h5 class="card-title">Detalles Perfil</h5>
 
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label ">Nombre Completo</div>
-                                        <div class="col-lg-9 col-md-8">{{ $user->name }}</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Cedula</div>
-                                        <div class="col-lg-9 col-md-8">{{ $user->cedula }}</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Rol</div>
-                                        <div class="col-lg-9 col-md-8">{{ $user->rol }}</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Email</div>
-                                        <div class="col-lg-9 col-md-8">{{ $user->email }}</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Telefono</div>
-                                        <div class="col-lg-9 col-md-8">{{ $user->telefono }}</div>
-                                    </div>
-
-                                </div>
-
-                                <div class="tab-pane fade profile-edit pt-3" id="profile-edit">
+                                <div class="tab-pane fade show active profile-edit" id="profile-edit">
 
                                     <!-- Profile Edit Form -->
 
-                                    <form>
+                                    <form method="POST" action="{{ route('users.update', $user->id) }}" role="form"
+                                        enctype="multipart/form-data">
+                                        {{ method_field('PATCH') }}
+                                        @csrf
                                         <div class="row mb-3">
                                             <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile
                                                 Image</label>
                                             <div class="col-md-8 col-lg-9">
-                                                <img src="assets/img/profile-img.jpg" alt="Profile">
+                                                <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Foto de Perfil">
                                                 <div class="pt-2">
                                                     <a href="#" class="btn btn-primary btn-sm"
                                                         title="Upload new profile image"><i class="bi bi-upload"></i></a>
@@ -199,59 +137,12 @@
 
                                 </div>
 
-                                <div class="tab-pane fade pt-3" id="profile-settings">
-
-                                    <!-- Settings Form -->
-                                    <form>
-
-                                        <div class="row mb-3">
-                                            <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email
-                                                Notifications</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="changesMade"
-                                                        checked>
-                                                    <label class="form-check-label" for="changesMade">
-                                                        Changes made to your account
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="newProducts"
-                                                        checked>
-                                                    <label class="form-check-label" for="newProducts">
-                                                        Information on new products and services
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="proOffers">
-                                                    <label class="form-check-label" for="proOffers">
-                                                        Marketing and promo offers
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="securityNotify"
-                                                        checked disabled>
-                                                    <label class="form-check-label" for="securityNotify">
-                                                        Security alerts
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary">Save Changes</button>
-                                        </div>
-                                    </form><!-- End settings Form -->
-
-                                </div>
-
                                 <div class="tab-pane fade pt-3" id="profile-change-password">
                                     <!-- Change Password Form -->
                                     <form>
 
                                         <div class="row mb-3">
-                                            <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Current
-                                                Password</label>
+                                            <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Contraseña Actual</label>
                                             <div class="col-md-8 col-lg-9">
                                                 <input name="password" type="password" class="form-control"
                                                     id="currentPassword">
@@ -259,8 +150,7 @@
                                         </div>
 
                                         <div class="row mb-3">
-                                            <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">New
-                                                Password</label>
+                                            <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">Nueva Contraseña</label>
                                             <div class="col-md-8 col-lg-9">
                                                 <input name="newpassword" type="password" class="form-control"
                                                     id="newPassword">
@@ -268,8 +158,7 @@
                                         </div>
 
                                         <div class="row mb-3">
-                                            <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter
-                                                New Password</label>
+                                            <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Confirmar la Nueva Contraseña</label>
                                             <div class="col-md-8 col-lg-9">
                                                 <input name="renewpassword" type="password" class="form-control"
                                                     id="renewPassword">
